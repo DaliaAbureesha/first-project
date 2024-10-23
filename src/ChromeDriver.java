0112d4539c0db99fcd6ef76afda095a564eb7ca4
DaliaAbureesha <dalia.aladwan@outlook.com>
2024-10-23 15:36:11 +0300

    update

---
+++ src/ChromeDriver.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+
+public class ChromeDriver {
+
+	public Object manage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void FindElement(By linkText) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void get(String myWebsite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object findElement(By linkText) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
